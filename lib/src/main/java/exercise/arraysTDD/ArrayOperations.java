d493725809fa6ea4a095a2297023159a5646803a
NikolayNedev <nikiyorozuya@gmail.com>
2021-10-07 10:41:44 +0200

    abgabe

--- lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
+++ lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
@@ -1,5 +1,7 @@
 package exercise.arraysTDD;
 
+import java.util.Arrays;
+
 /**
  * Utility class which performs various operations with integer arrays.
  */
@@ -22,7 +24,8 @@ public class ArrayOperations {
      */
 
     public void print() {
-        
+    	System.out.println(Arrays.toString(unsorted));
+    
     }
 
     /**
@@ -30,21 +33,48 @@ public class ArrayOperations {
      * @see <a href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
      */
     public int[] sort() {
-       return null;
+    	int temp;
+    	for (int i = 0; i < numbers.length; i++) {
+    		for (int j = i + 1; j < numbers.length; j++) {
+    			if(numbers[i] > numbers[j]) {
+    				temp = numbers[i];
+    				numbers[i] = numbers[j];
+    				numbers[j] = temp;
+    			}
+			}
+		}
+    	return numbers;
     }
 
     /**
      * @return the sorted array in reverse order
      */
     public int[] revertSort() {
-        return null;
+    	int temp;
+    	for (int i = 0; i < numbers.length; i++) {
+    		for (int j = i + 1; j < numbers.length; j++) {
+    			if(numbers[i] < numbers[j]) {
+    				temp = numbers[i];
+    				numbers[i] = numbers[j];
+    				numbers[j] = temp;
+    			}
+			}
+		}
+    	return numbers;
     }
 
     /**
      * @return the unsorted array in reverted order.
      */
     public int[] reverted() {
-        return null;
+    	int[]arr = new int[unsorted.length];
+    	int counter = 0;
+    	for (int i = unsorted.length -1; i >= 0; i--) {
+    		arr[counter] = unsorted[i];
+    		counter++;
+			
+		}
+        return arr;
     }
 
     /**
@@ -52,6 +82,12 @@ public class ArrayOperations {
      * @return true if the array contains the value, false otherwise.
      */
     public boolean contains(int value) {
+    	
+    	for (int i = 0; i < unsorted.length; i++) {
+    		if(unsorted[i] == value) {
+    			return true;
+    		}
+		}
         return false;
     }
 
@@ -59,28 +95,55 @@ public class ArrayOperations {
      * @return the average value of all elements summed up.
      */
     public double average() {
-        return 0.0;
+    	double avg = 0;
+    	for (int i = 0; i < numbers.length; i++) {
+    		avg = avg + numbers[i];
+		}
+        return avg / numbers.length;
     }
 
     /**
      * @return the average value of all elements summed up, but without the highest and the lowest value.
      */
     public double trimmedMean() {
-        return 0.0;
+		int max = numbers[0];
+		int min = numbers[0];
+		double sum = numbers[0];
+		for (int i = 1; i < numbers.length; i++) {
+			sum += numbers[i];
+			if (numbers[i] > max) {
+				max = numbers[i];
+			} else if (numbers[i] < min) {
+				min = numbers[i];
+			}
+		}
+		return ((sum - max - min) / (numbers.length - 2)) ;
     }
 
     /**
      * @return the max value of the array. In the array [1,9,3] max would be 9.
      */
     public int maxValue() {
-        return 0;
+		int max = numbers[0];
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] > max) {
+				max = numbers[i];
+			}
+		}
+		return max;
     }
 
     /**
      * @return the min value of the array. In the array [1,9,3] min would be 1.
      */
     public int minValue() {
-        return 0;
+		int min = numbers[0];
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] < min) {
+				min = numbers[i];
+			}
+		}
+		return min;
     }
 
     /* **********************************************************
